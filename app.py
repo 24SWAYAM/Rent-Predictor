2a1f8c420416eb494e004cab30bda3312687907c
24SWAYAM <96329142+24SWAYAM@users.noreply.github.com>
2024-04-13 16:09:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,20 @@ model = joblib.load("model.joblib")
 def home():
     return render_template("index.html")
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.form.to_dict() 
+    
+    # Check if all required keys are present in the input data
+    required_keys = ["seller_type", "bedroom", "layout_type", "property_type", "area", "furnish_type", "bathroom", "city", "localities"]
+    for key in required_keys:
+        if key not in data:
+            return render_template('prediction.html', predictions="Missing required input data. Please try again with all required fields.")
+    
+    # Convert data to DataFrame
     input_data = pd.DataFrame(data, index=[0]) 
     
+    # Map categorical features
     input_data["seller_type"] = input_data["seller_type"].map({'OWNER':0,'AGENT':1,'BUILDER':2})  
     input_data["bedroom"] = input_data["bedroom"].astype(int)
     input_data["layout_type"] = input_data["layout_type"].map({'BHK':0,'RK':1})
@@ -28,16 +36,20 @@ def predict():
     input_data["furnish_type"] = input_data["furnish_type"].map({'Furnished':0,'Semi-Furnished':1,'Unfurnished':2})
     input_data["bathroom"] = input_data["bathroom"].astype(int)
     input_data["city"] = input_data["city"].map({'Ahmedabad':0,'Bangalore':1,'Chennai':2,'Delhi':3,'Hyderabad':4,'Kolkata':5,'Mumbai':6,'Pune':7})   
-    encoded_localities = el.transform(input_data["localities"])
+      
+    
+    try:
+        # Make predictions
+        encoded_localities = el.transform(input_data["localities"])
+        input_data["localities"] = encoded_localities   
+        predictions = model.predict(input_data)
+        predictions = np.exp(predictions)
+        rounded_predictions = [round(prediction) for prediction in predictions]
+        return render_template('prediction.html', predictions=rounded_predictions[0])
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return render_template('xyz.html', predictions="Please select different locality as it is currently not available")
 
 
-    input_data["localities"] = encoded_localities     
-    predictions = model.predict(input_data)
-    predictions = np.exp(predictions)
-    # print(predictions,type(predictions))
-    predicted_prices = predictions.tolist()
-    rounded_predictions = [round(prediction) for prediction in predictions]
-    # print(type(rounded_predictions),rounded_predictions)
-    return render_template('prediction.html', predictions=rounded_predictions[0])
 if __name__ == '__main__':
     app.run(port=5500)
